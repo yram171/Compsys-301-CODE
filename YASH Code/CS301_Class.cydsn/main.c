9bc6ca8be0363b42df4f426008f7e54009644556
yram171 <yram171@aucklanduni.ac.nz>
2025-10-19 23:50:22 +1300

    aight

--- YASH Code/CS301_Class.cydsn/main.c
+++ YASH Code/CS301_Class.cydsn/main.c
@@ -117,10 +117,13 @@ static void light_sensors_update_and_maybe_request_turn(uint16_t* V4_pp, uint16_
 
 /* ================= PI Controller (same as your current file) ================= */
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 #define STEER_MAX        13
 #define KP               18.0f
 #define KI               2.0f
 =======
+=======
+>>>>>>> Stashed changes
 
 #define STEER_MAX        18
 #define KP               14.5f
@@ -130,6 +133,9 @@ static void light_sensors_update_and_maybe_request_turn(uint16_t* V4_pp, uint16_
 #define KP               18.0f
 #define KI               2.0f
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 #define INT_LIM          30.0f
 #define LOSS_TIMEOUT_T   0.25f
